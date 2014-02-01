fc97b0b008f596b32f498f7ac1778edd7a379c28
Sean Kelleher <sean@seankelleher.org>
2014-02-01 14:57:41 -0800

    Test update

--- test/simple_counter.v
+++ test/simple_counter.v
@@ -4,16 +4,13 @@ module simple_counter (
    counter_out
 );
 
-	input  		  CLOCK_5;
-	// WTF: why does the tutorial have us define both output and reg?  Which one wins? What's the dif? (memory?)
-//	output [31:0] counter_out;
-//	reg    [31:0] counter_out;
+	input             CLOCK_5;
    output reg [31:0] counter_out;
 
 
-	always @ (posedge CLOCK_5) // on positive clock edge
+	always @ (posedge CLOCK_5)
 		begin
-         counter_out <= counter_out + 1; // increment counter
+         counter_out <= counter_out + 1;
 		end
 
-endmodule // end of module counter
+endmodule
